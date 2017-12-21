22b7baba2aa68a93dbd146df729d61438a9663d2
kakapela <kacperkapela1@gmail.com>
2017-12-21 01:50:45 +0100

    Pierwsze GUI.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,20 @@
 package sample;
 
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
 public class Controller {
+
+    @FXML
+    private Label exit;
+
+    @FXML
+    void exitProgram(MouseEvent event) {
+System.exit(0);
+    }
+
+
+
 }
