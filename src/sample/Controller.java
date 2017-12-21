184f131e8a4a39a74538fb0914ef30ea87fb4ba0
kakapela <kacperkapela1@gmail.com>
2017-12-21 03:20:13 +0100

    Dodana walidacja inputow.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,16 +1,23 @@
 package sample;
 
 
+import com.jfoenix.controls.JFXDrawer;
+import com.jfoenix.controls.JFXHamburger;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
 import com.jfoenix.validation.RequiredFieldValidator;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -19,12 +26,7 @@ public class Controller implements Initializable {
 
 
     @FXML
-    private Label exit;
-
-    @FXML
-    void exitProgram(MouseEvent event) {
-System.exit(0);
-    }
+    private AnchorPane mainPane;
 
     @FXML
     private JFXTextField login;
@@ -32,9 +34,52 @@ System.exit(0);
     @FXML
     private JFXPasswordField haslo;
 
+    @FXML
+    private AnchorPane anchorPane2;
+
+    @FXML
+    private JFXDrawer drawer;
+
+    @FXML
+    private Label exit;
+
+    @FXML
+    private JFXHamburger hamburger;
+
+
+    @FXML
+    void exitProgram(MouseEvent event) {
+        System.exit(0);
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+
+        //hamburger transition
+        HamburgerBackArrowBasicTransition burgerTask2 = new HamburgerBackArrowBasicTransition(hamburger);
+        burgerTask2.setRate(-1);
+        hamburger.addEventHandler(MouseEvent.MOUSE_PRESSED, (e)->{
+            burgerTask2.setRate(burgerTask2.getRate()* -1);
+            burgerTask2.play();
+
+            if(drawer.isShown()){drawer.close();}
+            else {
+                drawer.open();
+            }
+        });
+
+        try {
+
+            VBox box = FXMLLoader.load(getClass().getResource("sideMenu.fxml"));
+            drawer.setSidePane(box);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+
         RequiredFieldValidator validator = new RequiredFieldValidator();
         RequiredFieldValidator validator2 = new RequiredFieldValidator();
 
