78766562361dbb3cb9782bdceec81ee55848f52a
daniel <daniel.talar@wp.pl>
2017-12-21 01:30:03 +0100

    Połaczenie z bazą z wykorzystaniem PreparedStatemant - poprawione zapytanie SQL

--- src/sample/Przyklad.java
+++ src/sample/Przyklad.java
@@ -6,7 +6,7 @@ public class Przyklad extends Query {
 
 
     public Przyklad() {
-        super("SELECT 'login','password' FROM users WHERE 'login' = ? AND 'password' = ? ");
+        super("SELECT * FROM users WHERE login = ? AND password = ? ");
 
     }
     @Override
@@ -14,18 +14,24 @@ public class Przyklad extends Query {
 
         myStmt=myConn.prepareStatement(sql);
 
-        //myStmt.setString(1,"ania");
-        //myStmt.setString(2,"bania");
-        System.out.println(myStmt);
-        resultSet= myStmt.executeQuery();
-        System.out.println(resultSet);
+        myStmt.setString(1,"ania");//login - docelowo pobieramy tu wartosci wpisane przez usera
+        myStmt.setString(2,"bania");//haslo
+        //System.out.println(myStmt);
+        resultSet= myStmt.executeQuery(); //wykonanie zapytania
+        //ystem.out.println(resultSet);
     }
 
     @Override
     protected void process() throws SQLException {
-        while (resultSet.next()){
+        if (resultSet.next()){//  sprawdzamy czy zapytanie zwrocilo cos - jesli tak to znaczy ze jest taki user w bazie, wiec logujemy go
 
             System.out.println("Zalogowałes sie");
+
+        }
+        else
+        {
+
+            System.out.println("Nie zalogowałes sie");
 
         }
 
