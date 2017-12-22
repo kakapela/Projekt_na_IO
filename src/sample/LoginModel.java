d869014105a5d564ee4d12c1345e0270b30761c9
kakapela <kacperkapela1@gmail.com>
2017-12-22 11:14:00 +0100

    Zapisanie Bazy.

---
+++ src/sample/LoginModel.java
@@ -0,0 +1,15 @@
+package sample;
+
+import java.sql.Connection;
+
+public class LoginModel {
+    public static Connection Connector(){
+
+        try{
+
+        }
+        catch (Exception e){}
+
+
+    }
+}
