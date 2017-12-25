6d03a7daeb9f7e0c55d6b1f43eb225d246e9e78c
kakapela <kacperkapela1@gmail.com>
2017-12-25 03:49:30 +0100

    Funkcja oddtwarzania została znacznie poprawiona.

--- src/sample/JavaController.java
+++ src/sample/JavaController.java
@@ -1,23 +1,32 @@
 package sample;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXSlider;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIconView;
 import javafx.application.Platform;
 import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
 import javafx.beans.binding.Binding;
 import javafx.beans.property.DoubleProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.Slider;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.stage.Modality;
 import javafx.stage.Window;
+import javafx.util.Duration;
 
 import javax.script.Bindings;
 import java.awt.event.ActionEvent;
@@ -35,8 +44,22 @@ public class JavaController implements Initializable{
     private Media me;
 
     @FXML
-    private JFXSlider volumeSlider;
+    private StackPane videoPane;
+    @FXML
+    private MaterialDesignIconView volume_down;
+    @FXML
+    private FontAwesomeIconView playpauseIcon;
+    @FXML
+    private JFXButton playpause;
+    @FXML
+    private FontAwesomeIconView volumeMax;
+    @FXML
+    private JFXSlider seekSlider;
 
+
+    @FXML
+    private JFXSlider volumeSlider;
+    boolean isPlaying=false;
     @FXML
     private AnchorPane mainPane;
     @Override
@@ -49,26 +72,77 @@ public class JavaController implements Initializable{
        // mp.setAutoPlay(true);
         DoubleProperty width= mv.fitWidthProperty();
         DoubleProperty height= mv.fitHeightProperty();
+        volume_down.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                mp.setVolume(0);
+                volumeSlider.setValue(0);
+                volume_down.setGlyphName("VOLUME_OFF");
+            }
+        });
+        volumeMax.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                mp.setVolume(100);
+                volumeSlider.setValue(100);
+            }
+        });
         volumeSlider.setValue(mp.getVolume()*100);
         volumeSlider.valueProperty().addListener(new InvalidationListener() {
             @Override
             public void invalidated(Observable observable) {
+                volume_down.setGlyphName("VOLUME_MEDIUM");
                 mp.setVolume(volumeSlider.getValue()/100);
             }
         });
-    }
-    public void play(){
-        mp.play();
+
+        playpause.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if(isPlaying){
+                    mp.pause();
+                    playpauseIcon.setGlyphName("PLAY");
+                    mp.setRate(1);
+                }
+                else{
+                    mp.play();
+                    playpauseIcon.setGlyphName("PAUSE_CIRCLE");
+                    mp.setRate(1);
+                }
+                isPlaying=!isPlaying;
+            }
+        });
+
+        mp.currentTimeProperty().addListener(new ChangeListener<Duration>() {
+            @Override
+            public void changed(ObservableValue<? extends Duration> observable, Duration oldValue, Duration newValue) {
+                seekSlider.setValue(newValue.toSeconds());
+            }
+        });
+        seekSlider.setValue(0);
+        seekSlider.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                mp.seek(Duration.seconds(seekSlider.getValue()));
+            }
+        });
+
 
     }
+
+    /*
+    public void play(){
+
+        mp.play();
+        mp.setRate(1);
+
+    }
+*/
     public void pause(){
         mp.pause();
 
     }
-    public void normalSpeed(){
-        mp.setRate(1);
 
-    }
     public void fast(){
         mp.setRate(1.25);
 
@@ -80,10 +154,12 @@ public class JavaController implements Initializable{
     public void reload(){
         mp.seek(mp.getStartTime());
         mp.play();
+        playpauseIcon.setGlyphName("PAUSE_CIRCLE");
     }
     public void start(){
         mp.seek(mp.getStartTime());
         mp.stop();
+        playpauseIcon.setGlyphName("PLAY");
 
     }
 
